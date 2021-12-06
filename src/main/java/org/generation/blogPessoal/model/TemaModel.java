5c03ee58f3da87392f4fa531eeab722b592b966c
unknown <gui.albuquerque.gui@gmail.com>
2021-12-06 11:54:03 -0300

    @guiAdaCruz [first commit]

---
+++ src/main/java/org/generation/blogPessoal/model/TemaModel.java
@@ -0,0 +1,54 @@
+package org.generation.blogPessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_tema")
+public class TemaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<PostagemModel> postagem;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public List<PostagemModel> getPostagem() {
+		return postagem;
+	}
+	public void setPostagem(List<PostagemModel> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+	
+}
